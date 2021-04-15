3186a27918a4e93f9121c43bc0f4ab5a37c71b8b
NATEYANA BLAKE <nblake707@gmail.com>
2021-04-15 14:27:03 -0400

    installed react-router redux & react-redux. Initial setup of the redux store

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/App';
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+// provider will inform all child components of state changes
+ReactDOM.render(<Provider store={store}> <App/> </Provider>
+, document.querySelector('#root'));
