aec699fc0704f4369e241af837a52a917b2cdb41
NATEYANA BLAKE <nblake707@gmail.com>
2021-04-20 16:56:20 -0400

    setup materialize css - began styling header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,8 @@
 import react from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
+import Header from './Header';
 
-const Header = () => <h2>Header</h2>
+
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
@@ -9,7 +10,7 @@ const Landing = () => <h2>Landing</h2>
 
 const App = () => {
     return (
-        <div>
+        <div className="container">
           <BrowserRouter>
             <div> 
               <Header />
@@ -18,7 +19,7 @@ const App = () => {
               <Route path="/surveys/new" component={SurveyNew} />
             </div> 
           </BrowserRouter>
-          
+
         </div>
     )
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,20 @@
+import react, { Component } from 'react';
+
+export default class Header extends Component {
+    render() {
+        return (
+           <nav>
+               <div className="nav-wrapper">
+                  <a className="left brand-logo">
+                      Emaily
+                  </a>
+                  <ul className="right">
+                      <li>
+                          <a>Login With Google</a>
+                      </li>
+                  </ul>
+               </div>
+           </nav>
+        )
+    }
+}
