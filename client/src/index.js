145448e3de8442775a97f678be45740aa6ad5f38
NATEYANA BLAKE <nblake707@gmail.com>
2021-04-21 16:39:06 -0400

    setup action creator and axios

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,13 @@ import react from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 
 import App from './components/App';
 import reducers from './reducers';
+
+// Action Creator - used to modify the state contained in redux store
 
 const store = createStore(reducers, {}, applyMiddleware());
 
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -6,4 +6,6 @@ module.exports = function (app) {
             target: "http://localhost:5000/",
         })
     );
+
+
 };
